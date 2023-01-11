23bb3e46c937f117e3f5009246ae099adaa1f150
young170 <seongbin10209@gmail.com>
2023-01-11 11:56:21 +0900

    Update framework

--- driver.c
+++ driver.c
@@ -13,29 +13,19 @@ main(void)
     while(1)
     {
         fprintf(stdout, "=> ");
-
         
         // Recieve input command.
         char *buf;
         int input_size = 32;
-        if(getline(&buf, input_size, stdin) == -1)
-        {
+        if (getline(&buf, input_size, stdin) == -1) {
             fprintf(stderr, "*** [Error] Reading command - input\n");
             continue;
         }
 
 
         // Handle parsing and execution.
-        if(interpertCmd(buf, strlen(buf)) == -1)
-        {
+        if (interpretCmd(buf, strlen(buf)) == -1) {
             fprintf(stderr, "*** [Error] Interpreting command");
-        }
-
-
-        if(getcwd(pwd, 1024) == NULL)
-        {
-            fprintf(stderr, "*** [Error] Listing current directory - ls\n");
-            continue;
         }
     }
     
--- shell.h
+++ shell.h
@@ -3,20 +3,31 @@
 // https://www.it-note.kr/209 - getcwd()
 // https://www.studymite.com/blog/strings-in-c#read_using_getline - getline()
 // https://stackoverflow.com/questions/37774983/clearing-the-screen-by-printing-a-character - clearing screen
-// 
+// https://sosal.kr/114 - ls command
 
 
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/wait.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <dirent.h>
+
+
+#ifdef DEBUG
+#define DPRINT(func) func;
+#else
+#define DPRINT(func) ;
+#endif
 
 
 void init_shell();	// Prints the information at the start of boot.
 void openHelp();	// Prints list of commmands.
-int interpertCmd(char** buf, size_t len);	// Parses and executes command.
+int interpretCmd(char **buf, size_t len);	// Parses and executes command.
+int parseCmd(char **buf, size_t len);
+int exCmd(char *option, char ***parseData);
+void list(char *path);
 
 
 char pwd[1024];
